4290df1f1e1c5a6e5eb8f9cf22ed020bbf766f1c
yanis aitlamine <yanis.aitlamine@gmail.com>
2024-08-04 20:11:57 +0200

    organize code into functions

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-""" 
-Basic example of scraping pipeline using SmartScraper
-"""
 from scrapegraphai.graphs import SearchLinkGraph
 from scrapegraphai.graphs import SmartScraperGraph
 from scrapegraphai.utils import prettify_exec_info
 import json
+
 # ************************************************
 # Define the configuration for the graph
 # ************************************************
-
-model ="civic"
-brand="honda"
 
 link_graph_config = {
     "llm": {
@@ -42,37 +37,61 @@ graph_config = {
     "verbose": True,  # Enable verbose mode for debugging purposes
 }
 
+model ="civic"
+brand="honda"
+
+# ************************************************
+# Define usefull functions
+# ************************************************
+
+def createCarsDict(car_data):
+    car_dict={}
+    for brand_info in car_data["brands"]:
+        brand_name = brand_info["brand"]
+        model_names = [model["name"] for model in brand_info["models"]]
+        car_dict[brand_name] = model_names
+    return car_dict
+
+def getLinks(brand, model, link_scraper_graph):
+    
+    link_scraper_graph.source="https://www.google.com/search?q="+brand+"+"+model+"+occasion"
+
+    result = link_scraper_graph.run()
+
+    output = json.dumps(result, indent=2)  # Convert result to JSON format with indentation
+
+    line_list = set(output.split("\n"))  # Split the JSON string into lines
+
+    return [line for line in line_list if (model in line and "google" not in line)]
+
+def getPriceFromLink(link):
+    smart_scraper_graph.source=link
+    result = smart_scraper_graph.run()
+    result["source"]=link
+    return result
+
+# Load JSON data from a file
+with open('TemplateInput.json', 'r') as file:
+    car_data = json.load(file)
+
+# Create the dictionary
+car_dict = createCarsDict(car_data)
+print(car_dict)
+
+# Initialize LinkScraperGraph with source, and configuration
+link_scraper_graph = SearchLinkGraph(
+        source="https://www.google.com/search?q="+brand+"+"+model+"+occasion",
+        config=link_graph_config
+    )
+
 # Initialize SmartScraperGraph with prompt, source, and configuration
 smart_scraper_graph = SmartScraperGraph(
-    #prompt="List all the content",  # Set prompt for scraping
     prompt="List me all the prices of the "+brand+" "+model+" along with the version, just give me the number no blank space or '€' characters, if the price is a price range like '11000-14000' only give me the average in that case it would me 12500 don't give me the price range",
-    # Source URL or HTML content to scrape
     source="",
-    config=graph_config  # Pass the graph configuration
+    config=graph_config
 )
 
-
-# ************************************************
-# Create the SearchLinkGraph instance and run it
-# ************************************************
-
-
-link_scraper_graph = SearchLinkGraph(
-    source="https://www.google.com/search?q="+brand+"+"+model+"+occasion",
-    config=link_graph_config
-)
-
-result = link_scraper_graph.run()
-print(result)
-
-output = json.dumps(result, indent=2)  # Convert result to JSON format with indentation
-
-line_list = set(output.split("\n"))  # Split the JSON string into lines
-
-filtered_lines = [line for line in line_list if (model in line and "google" not in line)]
-# Print each line of the JSON separately
-for line in filtered_lines:
-    print(line)
+links_list = getLinks(brand, model, link_scraper_graph)
     
 # ************************************************
 # Get graph execution info
@@ -81,10 +100,8 @@ for line in filtered_lines:
 graph_exec_info = link_scraper_graph.get_execution_info()
 print(prettify_exec_info(graph_exec_info))
 
-for line in filtered_lines:
-    smart_scraper_graph.source=line
-    result = smart_scraper_graph.run()
-    result["source"]=line
+for link in links_list:
+    result=getPriceFromLink(link)
     print (result)
     
     # Prettify the result and display the JSON
